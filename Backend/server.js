33d25ea477d0b65accf0ba5c7dc3425d0d1122fc
mehakMehak-cse <personalmehak9@gmail.com>
2026-08-14 17:43:54 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ const connectDB=require("./src/config/db")
 
 connectDB()
 
-app.listen(1000,()=>{
-    console.log("Server is running on port 1000")
-})
+const PORT = process.env.PORT || 1000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+});
